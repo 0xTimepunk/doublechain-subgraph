28b1f86e7303bd44de528f9ed2698f6d77c65e67
j-mars <jldcmartins@gmail.com>
2020-04-27 18:08:55 +0100

    minor script changes

--- mock/FundAccs.js
+++ mock/FundAccs.js
@@ -2,8 +2,14 @@
 module.exports = async function(done){
     let accounts =  await web3.eth.getAccounts();
     try {
-        await web3.eth.sendTransaction({from: accounts[0], to: "0xa9A2F37290dAE9152552FF4906af53Dc4E580fE0", value: web3.utils.toWei("1","ether")})
-        await web3.eth.sendTransaction({from: accounts[0], to: "0x2b912819318feb4e2399d85d697FAaB52faA90D7", value: web3.utils.toWei("1","ether")})
+        // portis
+        await web3.eth.sendTransaction({from: accounts[0], to: "0xff23bc46ad602101b02d857c867b7c7069dc908a", value: web3.utils.toWei("1","ether")})
+        // fortmatic
+        await web3.eth.sendTransaction({from: accounts[0], to: "0xBfAD455bDF38F154c0283bD6F9e5C9584dfe48d4", value: web3.utils.toWei("1","ether")})
+        // torus
+        await web3.eth.sendTransaction({from: accounts[0], to: "0xc6444E7c335d06c9570b243e457032530A1685A0", value: web3.utils.toWei("1","ether")})
+        // squarelink
+        await web3.eth.sendTransaction({from: accounts[0], to: "0x6d58b1d6e5a8b7a30944747d7495511cfe467c29", value: web3.utils.toWei("1","ether")})
 
     } catch (error) {
         console.log(error.message);

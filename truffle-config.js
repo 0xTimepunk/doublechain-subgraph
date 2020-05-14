dd0bc8fc7699a909f126ea669c2699f0585c22c4
j-mars <jldcmartins@gmail.com>
2020-05-14 16:36:43 +0100

    subgraph changes

--- truffle-config.js
+++ truffle-config.js
@@ -1,12 +1,9 @@
 // ganache-gui mnemonic sea huge rural lock garment antenna seek spatial glimpse name defy elder note: this mnemonic is not secure; don't use it on a public blockchain.
 const HDWalletProvider = require("@truffle/hdwallet-provider");
 const mnemonic = "surprise sweet upset clock glory used motor hover cross sight cry evidence"
-
 const path = require("path");
 module.exports = {
-
   plugins: ["truffle-security"],
-
   contracts_build_directory: path.join(__dirname, "src/abi"),
   networks: {
     ganachecli: {

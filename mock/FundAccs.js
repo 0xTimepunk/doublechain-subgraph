e92c573fba4e15606beee04ccf7e185275a98273
j-mars <jldcmartins@gmail.com>
2020-05-21 16:08:44 +0100

    Acc changes

--- mock/FundAccs.js
+++ mock/FundAccs.js
@@ -7,7 +7,7 @@ module.exports = async function(done){
         // fortmatic
         await web3.eth.sendTransaction({from: accounts[0], to: "0xBfAD455bDF38F154c0283bD6F9e5C9584dfe48d4", value: web3.utils.toWei("1","ether")})
         // torus
-        await web3.eth.sendTransaction({from: accounts[0], to: "0xc6444E7c335d06c9570b243e457032530A1685A0", value: web3.utils.toWei("1","ether")})
+        await web3.eth.sendTransaction({from: accounts[0], to: "0xFAB5CC5955fC6d4f8Caed172FBf9cfe3CB94426d", value: web3.utils.toWei("1","ether")})
         // squarelink
         await web3.eth.sendTransaction({from: accounts[0], to: "0x6d58b1d6e5a8b7a30944747d7495511cfe467c29", value: web3.utils.toWei("1","ether")})
         // walletConnect

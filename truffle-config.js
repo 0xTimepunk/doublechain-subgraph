55914ca2b1bb3dce495fd61c1c0358fd9a75f349
j-mars <jldcmartins@gmail.com>
2020-07-17 14:07:21 +0100

    clean repo

--- truffle-config.js
+++ truffle-config.js
@@ -1,69 +1,37 @@
-const ProviderEngine = require("web3-provider-engine");
-const WebsocketSubprovider = require("web3-provider-engine/subproviders/websocket.js");
-const { TruffleArtifactAdapter } = require("@0x/sol-trace");
-const { ProfilerSubprovider } = require("@0x/sol-profiler");
-const { RevertTraceSubprovider } = require("@0x/sol-trace");
-
-const mode = process.env.MODE;
-
-const projectRoot = "";
-const solcVersion = "0.5.17";
-const defaultFromAddress = "0x90F8bf6A479f320ead074411a4B0e7944Ea8c9C1";
-const isVerbose = true;
-const artifactAdapter = new TruffleArtifactAdapter(projectRoot, solcVersion);
-const provider = new ProviderEngine();
-
-if (mode === "profile") {
-  global.profilerSubprovider = new ProfilerSubprovider(
-    artifactAdapter,
-    defaultFromAddress,
-    isVerbose
-  );
-  global.profilerSubprovider.stop();
-  provider.addProvider(global.profilerSubprovider);
-  provider.addProvider(
-    new WebsocketSubprovider({ rpcUrl: "http://localhost:8546" })
-  );
-} else {
-  if (mode === "coverage") {
-    global.coverageSubprovider = new CoverageSubprovider(
-      artifactAdapter,
-      defaultFromAddress,
-      {
-        isVerbose,
-      }
-    );
-    provider.addProvider(global.coverageSubprovider);
-  } else if (mode === "trace") {
-    const revertTraceSubprovider = new RevertTraceSubprovider(
-      artifactAdapter,
-      defaultFromAddress,
-      isVerbose
-    );
-    provider.addProvider(revertTraceSubprovider);
-  }
-
-  provider.addProvider(
-    new WebsocketSubprovider({ rpcUrl: "http://localhost:8546" })
-  );
-}
-provider.start((err) => {
-  if (err !== undefined) {
-    console.log(err);
-    process.exit(1);
-  }
-});
-/**
- * HACK: Truffle providers should have `send` function, while `ProviderEngine` creates providers with `sendAsync`,
- * but it can be easily fixed by assigning `sendAsync` to `send`.
- */
-provider.send = provider.sendAsync.bind(provider);
-
+// ganache-gui mnemonic sea huge rural lock garment antenna seek spatial glimpse name defy elder note: this mnemonic is not secure; don't use it on a public blockchain.
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const mnemonic =
+  "surprise sweet upset clock glory used motor hover cross sight cry evidence";
+const path = require("path");
 module.exports = {
+  plugins: ["truffle-security"],
+  contracts_build_directory: path.join(__dirname, "src/abi"),
   networks: {
-    development: {
-      provider,
+    ganachecli: {
+      host: "127.0.0.1",
+      port: 8546,
       network_id: "*",
+      websockets: true,
+    },
+    ganachegui: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*",
+      websockets: true,
+    },
+    briing: {
+      provider: () =>
+        new HDWalletProvider(mnemonic, "https://demo:briing@briing.io/eth"),
+      network_id: "53006",
+      gas: "0x7a1200",
+    },
+  },
+  mocha: {
+    reporter: "eth-gas-reporter",
+    reporterOptions: {
+      currency: "USD",
+      coinmarketcap: "81874c1a-a4b3-4755-80ce-19baaac0ea82",
+      excludeContracts: ["Migrations"],
     },
   },
   compilers: {
